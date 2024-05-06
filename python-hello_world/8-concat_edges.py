f1c755cfbf8c232e3a7491fe292ccead43b820ad
Delphine-H <hannon.delphine@gmail.com>
2024-05-06 23:18:06 +0200

    use variables

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[41:59] + str[63:67] + str[:5])
+string = str[39:67] + str[-22:-17] + str[:7]
+print(string)
