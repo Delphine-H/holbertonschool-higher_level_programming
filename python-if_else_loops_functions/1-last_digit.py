9560b6721796ade2f2ed8b148e15a83441596292
Delphine-H <hannon.delphine@gmail.com>
2024-05-07 09:53:32 +0200

    fix error for number negative

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 
 number = random.randint(-10000, 10000)
 lastDigit = abs(number) % 10
-if lastDigit < 0:
+if number < 0:
     lastDigit = -lastDigit
 if lastDigit == 0:
     print(f"Last digit of {number} is {lastDigit} and is 0")
