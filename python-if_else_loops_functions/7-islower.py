6263b61880f05e361ff204b2010d08f47e32d942
Delphine-H <hannon.delphine@gmail.com>
2024-05-07 15:36:46 +0200

    update with if condition

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
 def islower(c):
-	return (ord(c) >= ord('a') and ord(c) <= ord('z'))
+	if ord(c) >= ord('a') and ord(c) <= ord('z'):
+		return (True)
+	else:
+		return (False)
+
