f74680c6e9bb9fbecccbdc7affb0240a38b4b314
Delphine-H <hannon.delphine@gmail.com>
2024-05-07 19:47:45 +0200

    Add function that adds two integers and returns the result

---
+++ python-if_else_loops_functions/10-main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def add(a, b):
+    """
+    Add two numbers.
+
+    Args:
+        a (int): The first number.
+        b (int): The second number.
+
+    Returns:
+        int: The sum of the two numbers.
+    """
+    return a + b
