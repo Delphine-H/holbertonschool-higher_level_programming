fcfe5944015ee6222bf3fc670954b862927ad6df
Delphine-H <hannon.delphine@gmail.com>
2024-05-07 20:00:11 +0200

    Add function that computes a to the power of b and return the value.

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def pow(a, b):
+    """
+    Compute the bitwise XOR of two numbers.
+
+    Args:
+        a (int): The base number.
+        b (int): The exponent number.
+
+    Returns:
+        int: The result of `a` raised to the power of `b`.
+    """
+    return a ^ b
