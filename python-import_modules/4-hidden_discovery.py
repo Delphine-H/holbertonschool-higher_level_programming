8136272bc7e1724a37aa332c6be74741dc191894
Delphine-H <hannon.delphine@gmail.com>
2024-05-09 00:55:55 +0200

    modif print format

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -4,4 +4,4 @@ import hidden_4  # type: ignore
 if __name__ == "__main__":
     for name in dir(hidden_4):
         if name[:2] != "__":
-            print(name)
+            print("{:s}".format(name))
