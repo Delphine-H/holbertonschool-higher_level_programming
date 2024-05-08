81dee49e746392dacdd71341d2d1bb39db5543b8
Delphine-H <hannon.delphine@gmail.com>
2024-05-09 00:41:11 +0200

    remove variable names

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,6 @@
 import hidden_4  # type: ignore
 
 if __name__ == "__main__":
-    names = dir(hidden_4)
-    for name in sorted(names):
-        if not name.startswith("__"):
+    for name in sorted(dir(hidden_4)):
+        if "__" not in name:
             print(name)
