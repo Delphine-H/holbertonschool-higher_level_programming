0a6777020a8e62f83a1f7a48b2a0b7dfccd749ab
Delphine-H <hannon.delphine@gmail.com>
2024-05-14 20:54:19 +0200

    modification to adhere PEP standards

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,8 @@ def roman_to_int(roman_string):
     if (type(roman_string) is not str) or (roman_string is None):
         return 0
 
-    roman_numeral = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roman_numeral = {"I": 1, "V": 5, "X": 10, "L": 50,
+                     "C": 100, "D": 500, "M": 1000}
     result = 0
     previous = 0
 
