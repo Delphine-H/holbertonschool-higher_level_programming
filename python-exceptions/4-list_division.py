04f7589dbd11f16385b1c2098c4abd8c8bc2c455
Delphine-H <hannon.delphine@gmail.com>
2024-05-15 15:54:31 +0200

    Use finally

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -15,4 +15,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
             division.append(0)
+        finally:
+            pass
     return division
