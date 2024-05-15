b574155490333b487734be3aad6dd2fb1088fc78
Delphine-H <hannon.delphine@gmail.com>
2024-05-15 13:10:43 +0200

    Moved the try-except block inside the loop to catch only IndexError

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,12 +3,13 @@
 
 def safe_print_list(my_list=[], x=0):
     print_element = 0
-    try:
-        for element in range(x):
+    for element in range(x):
+        try:
             print("{}".format(my_list[element]), end="")
             print_element += 1
-        print()
-        return print_element
-    except:
-        print()
-        return print_element
+        except:
+            break
+
+    print()
+
+    return print_element
