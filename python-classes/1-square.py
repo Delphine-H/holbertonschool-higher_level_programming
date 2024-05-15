9f1530e4d348abe426d6380b8a1df3c76b4cf162
Delphine-H <hannon.delphine@gmail.com>
2024-05-15 17:36:41 +0200

    change if condition

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,7 +6,7 @@ class Square:
     """A Square class that devine a square"""
 
     def __init__(self, size=0):
-        if type(size) != int:
+        if type(size) is not int:
             raise TypeError("size must be integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
