b70d0a486d93b72cfd320a27a8d1fd591b98074d
Delphine-H <hannon.delphine@gmail.com>
2024-05-15 17:30:55 +0200

    Add documentation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -10,4 +10,5 @@ class Square:
             raise TypeError("size must be integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
+        """instance attribute"""
         self.__size = size
