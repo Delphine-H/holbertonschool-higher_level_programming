d1ea7cf06767e3480627f3df02ca31b0ba64533c
Delphine-H <hannon.delphine@gmail.com>
2024-05-15 17:46:21 +0200

    Add class Square that defines a square

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A class Square"""
+
+
+class Square:
+    """A Square class that devine a square"""
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        """instance attribute"""
+        self.__size = size
