eab089bdac07db7f33e991c880dfe00e874d3c2c
Delphine-H <hannon.delphine@gmail.com>
2024-05-21 22:04:43 +0200

    fix typing mistake

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -27,9 +27,12 @@ def matrix_divided(matrix, div):
     if (not isinstance(matrix, list)) or not all(
         isinstance(row, list) for row in matrix
     ):
-        raise TypeError("matrix must be a matrix (list or lists)"
+        raise TypeError("matrix must be a matrix (list of lists)"
                         " of integers/floats")
-    if len(matrix) == 0 or any(len(row) != len(matrix[0]) for row in matrix):
+    if len(matrix) == 0 or any(len(row) == 0 for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists)"
+                        " of integers/floats")
+    if any(len(row) != len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
