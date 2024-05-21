dbaaef1a734ecfce31de41358b3fb428577a29a4
Delphine-H <hannon.delphine@gmail.com>
2024-05-21 14:30:58 +0200

    refactor code and change documentation

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -9,11 +9,11 @@ after converting them to integers.
 def add_integer(a, b=98):
     """
     Adds two integers or floats after converting them to integers.
-
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer or float")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer or float")
-
-    return int(a) + int(b)
+    a = int(a)
+    b = int(b)
+    return a + b
