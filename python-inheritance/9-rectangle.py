a5491680a07efedacd280dd8cef7e52757f5ae36
Delphine-H <hannon.delphine@gmail.com>
2024-05-24 01:01:54 +0200

    Add area method and representation

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module contain a class Rectangle that inherit
+from BaseGeometry
+"""
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Class Rectangle that inherit from BaseGeometry
+    """
+
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("height", height)
+        self.integer_validator("width", width)
+
+    def aera(self):
+        """Methode that eturn the area"""
+        return self.__width + self.__height
+
+    def __str__(self):
+        """This method returns a string representation"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
