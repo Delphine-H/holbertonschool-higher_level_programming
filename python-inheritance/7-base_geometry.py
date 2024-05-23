c81cf638f3957b63feae53c0fb2e700a19bdabc4
Delphine-H <hannon.delphine@gmail.com>
2024-05-23 21:59:41 +0200

    correct int condition

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -15,7 +15,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """methode that validate value"""
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
