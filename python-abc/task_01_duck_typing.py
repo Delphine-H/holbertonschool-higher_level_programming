0e58071da6b4149f04f0e2a5d1fff0cb232c0052
Delphine-H <hannon.delphine@gmail.com>
2024-05-24 16:05:43 +0200

    Add Class Shape and 2 subclass Circle and Rectangle

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+Module that contain a class Shape and 2 subclass
+Circle and rectangle
+"""
+
+from abc import ABC, abstractmethod
+import math
+
+
+class Shape(ABC):
+    """
+    Class shape that inherit ABC
+    """
+
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    """
+    Class Circle that inherit Shape
+    """
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius**2
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
+
+class Rectangle(Shape):
+    """
+    Class Rectangle that inherit Shape
+    """
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+
+def shape_info(shape):
+    """
+    Standalone function shape_info
+    """
+    print("Aera:", shape.area())
+    print("Perimeter:", shape.perimeter())
