6020114248bc285df780837fdabdab418fd6c3a3
Delphine-H <hannon.delphine@gmail.com>
2024-05-24 16:52:11 +0200

    modification to adhere PEP standards

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -28,7 +28,7 @@ class Circle(Shape):
     """
 
     def __init__(self, radius):
-        if self.radius < 0:
+        if radius < 0:
             raise ValueError("radius must be >= 0")
         self.radius = radius
 
@@ -45,9 +45,9 @@ class Rectangle(Shape):
     """
 
     def __init__(self, width, height):
-        if self.width < 0 :
+        if width < 0:
             raise ValueError("width must be >= 0")
-        if self.height < 0:
+        if height < 0:
             raise ValueError("height must be >= 0")
         self.width = width
         self.height = height
