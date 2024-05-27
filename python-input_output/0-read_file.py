b36bae60d4a339ece51633a924c647d53ae9d6e5
Delphine-H <hannon.delphine@gmail.com>
2024-05-27 11:23:43 +0200

    print without /n

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -7,4 +7,5 @@ This module contain a function read_file
 def read_file(filename=""):
     """function that open a file"""
     with open(filename, encoding="utf-8") as my_file:
-        pass
+        for l in my_file:
+            print(l, end="")
