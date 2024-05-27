a7f7fd7d32a69b36329fdf74e70adb2cef7a24a6
Delphine-H <hannon.delphine@gmail.com>
2024-05-27 11:26:43 +0200

    change variable name

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ This module contain a function read_file
 def read_file(filename=""):
     """function that open a file"""
     with open(filename, encoding="utf-8") as my_file:
-        for l in my_file:
-            print(l, end="")
+        for my_line in my_file:
+            print(my_line, end="")
