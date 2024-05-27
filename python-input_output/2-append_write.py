1c5fdff6ff75786800d2514e9a2fc5585d9ff45f
Delphine-H <hannon.delphine@gmail.com>
2024-05-27 15:52:53 +0200

    add documentation

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-
+"""
+This module contain a function append_write
+"""
 
 def append_write(filename="", text=""):
+    """function that append text at the end of the file"""
     with open(filename, "a", encoding="utf8") as my_file:
         return my_file.write(text)
