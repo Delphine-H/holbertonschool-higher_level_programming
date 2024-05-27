75945538a089907b8df42b3702d67795f5ed575d
Delphine-H <hannon.delphine@gmail.com>
2024-05-27 11:20:35 +0200

    Add function that reads a test file and prints it to sdout

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module contain a function read_file
+"""
+
+
+def read_file(filename=""):
+    """function that open a file"""
+    with open(filename, encoding="utf-8") as my_file:
+        pass
