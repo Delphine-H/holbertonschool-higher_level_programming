d5e9146689a4da02a51adeb2c069efb045a9698b
Delphine-H <hannon.delphine@gmail.com>
2024-05-28 12:37:19 +0200

    Move file in right directory

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module contain a function pascal_triangle"""
+
+
+def pascal_triangle(n):
+    """function that generate a Pascal's triangle"""
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[1 - 1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
