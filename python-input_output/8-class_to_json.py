d0c54cc7c91b93a15bad999de24a25fc0eeb61a1
Delphine-H <hannon.delphine@gmail.com>
2024-05-28 10:27:58 +0200

    modification to adhere PEP standards

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -5,5 +5,5 @@ This module contain a function class_to_json
 
 
 def class_to_json(obj):
-	"""function that returns the dictionary description of an object"""
-	return vars(obj)
+    """function that returns the dictionary description of an object"""
+    return vars(obj)
