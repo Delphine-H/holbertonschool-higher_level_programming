fdcd313ed8e755dc608a928b536dcee1cfce6ae3
Delphine-H <hannon.delphine@gmail.com>
2024-06-21 00:13:17 +0200

    remove set password

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -5,9 +5,6 @@
 -- Create user if not exists
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- Set Password for User user_0d_1
-SET PASSWORD FOR 'user_0d_1'@'localhost' = PASSWORD('user_0d_1_pwd');
-
 -- Grant all privileges to the user
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
