a428d4d6c97960a2be32c86a4a7f1b309b414f0f
Delphine-H <hannon.delphine@gmail.com>
2024-06-21 14:04:05 +0200

    Refactor code

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 """
-Script that lists all cities from the database hbtn_0e_4_usa
+Script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument
+but safe from MySQL injections!
 """
 import MySQLdb
 from sys import argv
 
 # The code should not be executed when imported
 if __name__ == "__main__":
+
     # make a connection to the database
     db = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
     )
 
+    # It gives us the ability to have multiple seperate working environments
+    # through the same connection to the database.
     cur = db.cursor()
-    cur.execute(
-        "SELECT cities.id, cities.name, states.name FROM cities\
-                INNER JOIN states ON cities.state_id = states.id\
-                ORDER BY cities.id ASC"
-    )
+    cur.execute("SELECT * FROM states WHERE BINARY name = %s", [argv[4]])
 
     rows = cur.fetchall()
     for i in rows:
@@ -25,3 +26,4 @@ if __name__ == "__main__":
     # Clean up process
     cur.close()
     db.close()
+    
