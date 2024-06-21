d6e3db21c2eac568358e5cb91953f80ef1795527
Delphine-H <hannon.delphine@gmail.com>
2024-06-21 10:32:12 +0200

    Add script that lists all cities contained in the database

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,11 @@
+-- Select the id, name of cities and the corresponding state name using a subquery
+-- The subquery finds the state name based on the state_id in the cities table
+-- Results are ordered by cities.id in ascending order
+SELECT 
+    cities.id, 
+    cities.name, 
+    (SELECT states.name FROM states WHERE states.id = cities.state_id) AS state_name 
+FROM 
+    cities 
+ORDER BY 
+    cities.id ASC;
