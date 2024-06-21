22adebba1b9eae5e0680c9252603fd575728f5a1
Delphine-H <hannon.delphine@gmail.com>
2024-06-21 12:46:50 +0200

    Add a script that lists all states from the database hbtn_0e_0_usa

---
+++ python-object-relational_mapping/0-select_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+# Check if all required arguments are provided
+if len(sys.argv) != 4:
+    print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+    sys.exit(1)
+
+# Retrieve command-line arguments
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+
+#print("Connecting to MySQL with user={}, password={}, database={}".format(username, password, database))
+
+try:
+    # Connect to MySQL
+    db = MySQLdb.connect(
+        host='localhost',
+        user=username,
+        passwd=password,
+        db=database
+    )
+    print("Successfully connected to MySQL!")
+
+    # Further logic to execute queries can be added here
+    cursor = db.cursor()
+
+    # Example query to fetch and print all states
+    cursor.execute("SELECT * FROM states ORDER BY id")
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
+
+except MySQLdb.Error as e:
+    print(f"Error connecting to MySQL: {e}")
+    sys.exit(1)
