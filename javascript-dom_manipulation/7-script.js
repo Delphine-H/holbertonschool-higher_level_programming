881d7b04bf6235c565867ad2d8929a082af65ffe
Delphine HANNON <hannon.delphine@gmail.com>
2024-07-24 16:32:45 +0200

    Modifications to adhere ES6 standards

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -6,7 +6,7 @@ const listMoviesElement = document.querySelector('#list_movies');
 
 // Fetch the data from the API
 fetch(apiUrl)
-  .then(response => {
+  .then((response) => {
     // Check if the response is ok (status code 200-299)
     if (!response.ok) {
       throw new Error('Network response was not ok');
@@ -14,23 +14,23 @@ fetch(apiUrl)
     // Parse the JSON from the response
     return response.json();
   })
-  .then(data => {
+  .then((data) => {
     // Extract the list of films from the data
     const films = data.results;
     // Create a document fragment to hold the list items
     const fragment = document.createDocumentFragment();
-    
+
     // Iterate over each film and create a list item
-    films.forEach(film => {
+    films.forEach((film) => {
       const li = document.createElement('li');
       li.textContent = film.title;
       fragment.appendChild(li);
     });
-    
+
     // Append the fragment to the ul element
     listMoviesElement.appendChild(fragment);
   })
-  .catch(error => {
+  .catch((error) => {
     // Handle any errors that occurred during the fetch
     console.error('There has been a problem with your fetch operation:', error);
     listMoviesElement.innerHTML = '<li>Failed to load movie data</li>';
